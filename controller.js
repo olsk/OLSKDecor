4ded431683b876b2c36048d3bc99704d95761297
Rosano Coutinho <pub@xeari.com>
2020-12-23 18:52:04 -0300

    Scaffold controller

---
+++ controller.js
@@ -0,0 +1,23 @@
+const mod = {
+
+	OLSKControllerRoutes  () {
+		return [{
+			OLSKRoutePath: '/',
+			OLSKRouteMethod: 'get',
+			OLSKRouteSignature: 'OLSKDecorRoute',
+			OLSKRouteFunction (req, res, next) {
+				return res.send(require('fs').readFileSync('index.html', 'utf-8').replace('</head>', `<script>
+  document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>')
+</script></head>`));
+			},
+		}];
+	},
+
+	OLSKControllerUseLivereload () {
+		return true;
+	},
+
+};
+
+Object.assign(exports, mod)
